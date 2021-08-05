f6c6bea360366bb6e9c8862e1fb6f8fe1fe70bb9
wdev733 <wdev733@gmail.com>
2021-08-05 13:30:02 -0700

    home page, fix footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,6 +14,11 @@ import "../assets/scss/main.scss"
 // const Carousel = loadable(() => import('@brainhubeu/react-carousel'))
 // const slidesToShowPlugin = Carousel.slidesToShowPlugin
 
+import welcomeBg from '@images/welcome.png'
+import spiral1Img from '@images/spiral-1.png'
+import spiral2Img from '@images/spiral-2.png'
+import coinsImg from '@images/coins.png'
+
 const frameImg = '/images/Frame.png'
 const etheriumImg = '/images/etherium.png'
 const etheriumImg1 = '/images/etherium-1.png'
@@ -222,256 +227,79 @@ export default function Main() {
       <div className='main-top'>
         <div className='main-top__left-container'>
           <div>
-            <h1 className='text-uppercase d-lg-block d-md-none d-block'>
-              Welcome to<br/>Sommelier<br />the <span >new coprocessor</span><br />for Ethereum
+            <p className='new'>NEW</p>
+            <h1 className='text-uppercase d-lg-block d-md-none d-block sommelier'>
+              Sommelier
             </h1>
-            <h1 className='text-uppercase d-lg-none d-md-block d-none'>
-              Welcome to Sommelier the <span >new coprocessor</span><br />for Ethereum
+            <h1 className='text-uppercase d-lg-block d-md-none d-block cellars'>
+              CELLARS
             </h1>
-            <p className='caption-text-small mt-3'>Move your DeFi assets into higher<br />yields faster and cheaper</p>
+            <p className='caption-text-small mt-3'>Automated Liquidity Management<br/>for Decentralized Finance</p>
             <a href="https://app.sommelier.finance" className='launch-button' target="_blank">
-              <img src={frameImg} alt='frame image' className='mr-2'/>
-              Launch Application
+              <span>Add Liquidity</span>
+              <img src={coinsImg} alt='frame image' className='mr-2'/>
             </a>
           </div>
         </div>
         <div className='main-top__right-container'>
-          <img src={etheriumImg} alt='frame image' />
+          <img src={spiral2Img} alt='frame image' />
         </div>
       </div>
       <a name="features" id='features'></a>
-      <div className='main-why section-container'>
-        <div className='main-top__left-container'>
-          <h1 className='text-uppercase'>
-            Why <span >Sommelier</span>
-          </h1>
-        </div>
-        <div className='main-why__content row'>
-          <div className='main-why__content__img-container'>
-            <img src={etheriumImg1} alt='etherium image' className='mr-2 main-why__content__img-container__img'/>
-            <img src={etheriumMobileImg} alt='etherium image' className='mr-2 main-why__content__img-container__img-mobile' width='74'/>
+
+      <div className="main-welcome" style={{ backgroundImage: `url(${welcomeBg})`, backgroundSize: 'cover' }}>
+        <img src={etheriumImg} alt='frame image' />
+        <h2>Welcome to Sommelier</h2>
+        <p>The new coprocessor for the Ethereum VM</p>
+        <a href='#' className='add-liquidity-button'>Add Liquidity</a>
+      </div>
+
+      <div className='main-why'>
+        <h2>
+          Why Sommelier <span>Cellars</span>?
+        </h2>
+        <div className='main-why__content'>
+          <div className='main-why__content-item'>
+            <h4>Liquidity Provider Management Tools</h4>
+            <p>Sommelier Cellars are focused on Uniswap v3 which has high degree of capital efficiency in the Range Order structure.</p>
           </div>
-          <div className='main-why__right-container'>
-            <ul>
-              <li>
-                <img src={cosmosImg} alt='Cosmos Image' />
-                <h4>Cosmos Stargate SDK</h4>
-                <p>Modular and robust protocol with Tendermint Consensus delivers world class and tested protocol layer</p>
-              </li>
-              <li>
-                <img src={etherbridgeImg} alt='Ether Bridge Image' />
-                <h4>Bi-Directional Ethereum Bridge</h4>
-                <p>Ethereum transactions are managed by the most functional bridge optimized for extending Ethereum features for users</p>
-              </li>
-              <li>
-                <img src={secureImg} alt='Secure Image' />
-                <h4>Secure</h4>
-                <p>Transactions are managed by the Sommelier validator set with decentralized governance. Sommelier cannot steal user funds</p>
-              </li>
-              <li>
-                <img src={automatedImg} alt='Automated Image' />
-                <h4>Automated</h4>
-                <p>Automated signature management for Ethereum transactions enable automated DeFi trading for increased liquidity momentum and yield capture</p>
-              </li>
-            </ul>
+          <div className='main-why__content-item'>
+            <h4>Bi-Directional Ethereum Bridge</h4>
+            <p>Ethereum transactions are managed by the most functional bridge optimized for extending Ethereum features for Liquidity Providers.</p>
+          </div>
+          <div className='main-why__content-item'>
+            <h4>Data-driven management strategies</h4>
+            <p>Sommelier validators leverage optimal liquidity management strategies based on blockchain transaction data and advanced price prediction models.</p>
+          </div>
+          <div className='main-why__content-item'>
+            <h4>Non-Custodial Liquidity Oracle</h4>
+            <p>Sommelier transactions are managed by the Sommelier blockchain validator set with decentralized governance. Sommelier itself does not control user funds.</p>
           </div>
         </div>
       </div>
       <a name="usecases" id='usecases'></a>
       <div className='main-usecases section-container'>
-        <div className='main-top__left-container'>
-          <h2 className='text-uppercase'>
-            Use Cases
-          </h2>
-        </div>
+        <h2>
+          Use Cases
+        </h2>
         <div className='main-usecases__content'>
-          {isMobileOnly ? (
-            <div style={{width: carsouselUsecasesWidth, display: 'flex'}}>
-            <ul className='main-usecases__content__carousel'
-              // plugins={[
-              //   'centered',
-              //   {
-              //     resolve: slidesToShowPlugin,
-              //     options: {
-              //       numberOfSlides: 1,
-              //     },
-              //   },
-              // ]}
-            >
-              {usecasesData.map((item, index) => (
-                <li style={{width: windowDimensions.width}}>
-                <div>
-                  <img src={item.img} width='106' />
-                  <h3>{item.title}</h3>
-                  <p>{item.description}</p>
-                </div>
-                </li>
-              ))}
-            </ul>
-            </div>
-          ) : (
-            <ul>
-              {usecasesData.map((item, index) => (
-                <li>
-                  <div>
-                    <img src={item.img} width='106' />
-                    <h3>{item.title}</h3>
-                    <p>{item.description}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-      <a name="roadmap" id="roadmap"></a>
-      <div className='main-roadmap'>
-        <div className='main-top__left-container'>
-          <h2 className='text-uppercase'>
-            Roadmap
-          </h2>
-        </div>
-        <div className='main-roadmap__content'>
-          <div className='main-roadmap__content__q-left'>
-            <div>
-              <h3>Q2 2021</h3>
-              <ul>
-                <li><span>Sommelier Testnet Alpha 1 Live</span></li>
-                <li>Sommelier Testnet Audit complete<br /></li>
-                <li>Sommelier Testnet Alpha 2 Live<br /></li>
-                <li>Sommelier Public Validator Invite<br /></li>
-                <li>Sommelier Mainnet 1 Live</li>
-              </ul>
-            </div>
+          <div className='main-usecases__content-item'>
+            <h3>Decentralized Portfolio<br/>Management</h3>
+            <p>Validator set portfolio management with<br/>full user funds control.</p>
           </div>
-          <img className='main-roadmap__content__stick' src={stickImg} width='35' height='794'></img>
-          <img className='main-roadmap__content__stick-mobile' src={stickMobileImg} width='30' height='824'></img>
-          <div className='main-roadmap__content__q-right'>
-            <div className='main-roadmap__content__q-right__1'>
-              <h3>Q1 2021</h3>
-              <ul>
-                <li><span>Uniswap and Ethereum Oracles on Cosmos SDK<br /></span></li>
-                <li>Ethereum Liquidity Logic implemented on Gravity<br /></li>
-                <li>Ethereum and Cosmos wallet integration complete<br /></li>
-                <li>Sommelier Impermanent Loss tracking app live</li>
-              </ul>
-            </div>
-            <div className='main-roadmap__content__q-right__3'>
-              <h3>Q2 2021</h3>
-              <ul>
-                <li>Sommelier Testnet Alpha 1 Live<br /></li>
-                <li>Sommelier Testnet Audit complete<br /></li>
-                <li>Sommelier Testnet Alpha 2 Live<br /></li>
-                <li>Sommelier Public Validator Invite<br /></li>
-                <li>Sommelier Mainnet 1 Live</li>
-              </ul>
-            </div>
-            <div className='main-roadmap__content__q-right__2'>
-              <h3>Q3 2021</h3>
-              <ul>
-                <li>Sommelier Automated DeFi Selections on Uniswap<br /></li>
-                <li>Sommelier Automated DeFi Selections on Sushiswap<br /></li>
-                <li>Sommelier Automated DeFi Selections on Binance Chain<br /></li>
-                <li>Sommelier DAO launches</li>
-              </ul>
-            </div>
+          <div className='main-usecases__content-item'>
+            <img src={spiral1Img} />
+            <h3>Compounding Fees Across<br/>AMMs and Networks</h3>
+            <p>Automatically compounding fees without<br/>need for active intervention.</p>
+          </div>
+          <div className='main-usecases__content-item'>
+            <h3>Automated Portfolio<br/>Rebalancing</h3>
+            <p>Automatically rebalance portfolio to<br/>focus on high yield Liquidity Provider<br/>opportunities and exit lower yielding<br/>opportunities.</p>
           </div>
         </div>
       </div>
-      <div className='main-about section-container'>
 
-        <div className='main-about__left'>
-          <div className='main-about__left__img-container'/>
-        </div>
-        <div className='main-about__right'>
-          <a name="about" id="about"></a>
-          <h2>About</h2>
-
-          <p>
-          Sommelier is a bet that Ethereum will be a dominant
-          player in the global economy. Sommelier consists of the
-          Cosmos Stargate SDK, its Tendermint-based consensus
-          layer and a decentralized, bi-directional Ethereum bridge,
-          managed by a global network of validators.
-          <br /><br />
-          Liquidity Providers (LPs) will be able to use the Sommelier
-          to author and execute complex, and automated financial
-          transactions, such as portfolio rebalancing, limit orders,
-          batched orders, as well as a host of other features that
-          traders have come to expect from CeFi, but that are not
-          currently available in DeFi.
-          </p>
-
-          <a href="https://app.sommelier.finance" className='launch-button' style={{ marginTop: '4.063rem'}} target="_blank">
-          <img src={frameImg} alt='frame image' className='mr-2'/>
-            Launch Application
-          </a>
-        </div>
-      </div>
-      <a name="team" id='team'></a>
-      <div className='main-team section-container'>
-        <h2>Team</h2>
-        <div className='main-team__top'>
-          {teamMembers1 && (
-            <ul>
-              {teamMembers1.map((item, index) => (
-                <li>
-                  <Profile data={item} isPortrait={true} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className='main-team__down'>
-          {teamMembers2 && (
-            <ul>
-              {teamMembers2.map((item, index) => (
-                <li>
-                  <Profile data={item} isPortrait={false} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className='main-team__all'>
-          {isMobileOnly ? (
-            <div className='main-team__all__carousel' style={{width: carsouselWidth, display: 'flex'}}
-              // plugins={[
-              //   'centered',
-              //   {
-              //     resolve: slidesToShowPlugin,
-              //     options: {
-              //       numberOfSlides: 1.5,
-              //     },
-              //   },
-              // ]}
-            >
-              {teamMembers3.map((item, index) => (
-                <div style={{width: teamItemWidth, display: 'flex', justifyContent: 'center'}}>
-                  <Profile data={item} isPortrait={index < 6 ? true : isMobileOnly ? true : false} />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <ul>
-              {teamMembers3.map((item, index) => (
-                <li style={index < 6 ? {width: '50%'} : {width: '100%'}}>
-                  <Profile data={item} isPortrait={index < 6 ? true : isMobileOnly ? true : false} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-      <div className='main-investors section-container'>
-        <h2 className="text-center">INVESTORS</h2>
-        <div className='main-investors__img-container'>
-          <img src={cygnilabsImg} width='112' />
-          <img src={standardImg} width='134' />
-          <img src={multicoinImg} width='271' />
-          <img src={alamedaImg} width='261' />
-        </div>
-      </div>
+      <div className='main-divider'></div>
     </div>
   )
 }
