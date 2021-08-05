de33054c70230130d57aa5f98e1b86135f1eb07d
wdev733 <wdev733@gmail.com>
2021-08-05 13:30:02 -0700

    home page, fix footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,15 @@ module.exports = {
       options: {
         plugins: [`gatsby-remark-autolink-headers`],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@images": `${__dirname}/src/images`,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
