a8be1af586be278f9890f1b50833e959f300383e
wdev733 <wdev733@gmail.com>
2021-08-10 13:26:04 -0700

    mobile responsive, bottom cellar bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,6 +18,11 @@ import welcomeBg from '@images/welcome.png'
 import spiral1Img from '@images/spiral-1.png'
 import spiral2Img from '@images/spiral-2.png'
 import coinsImg from '@images/coins.png'
+import coinsWhiteImg from '@images/coins-white.png'
+
+import ethImg from '@images/coins/eth.png'
+import usdcImg from '@images/coins/usdc.png'
+import usdtImg from '@images/coins/usdt.png'
 
 const etheriumImg = '/images/etherium.png'
 
@@ -43,28 +48,6 @@ const whyData = [
   {
     title: 'Non-Custodial Liquidity Oracle',
     description: 'Sommelier transactions are managed by the Sommelier blockchain validator set with decentralized governance. Sommelier itself does not control user funds.',
-  },
-]
-const usecasesData = [
-  {
-    img: imperImg,
-    title: 'Impermanent Loss Protection',
-    description: 'Automatically rebalance your portfolio when pool performance changes such that Impermanent and unrealized losses require a change in strategy',
-  },
-  {
-    img: rebalancingImg,
-    title: 'Automated Portfolio Rebalancing',
-    description: 'Automatically rebalance portfolio to focus on high yield Liquidity Provider opportunities and exit lower yielding opportunities',
-  },
-  {
-    img: lowerImg,
-    title: 'Lower Ethereum Gas Transaction Fees',
-    description: 'Aggregating and batching transactions with Sommelier will yield lower gas prices for LP growing demand to manage pool position and performance',
-  },
-  {
-    img: greaterImg,
-    title: 'Greater Liquidity Momentum',
-    description: 'Native transaction batching and roll-ups deliver greater liquidity momentum to capture onchain yield on the Ethereum blockchain',
   },
 ]
 
@@ -131,30 +114,26 @@ export default function Main() {
       </div>
       <a name="features" id='features'></a>
 
-      <div className="main-welcome" style={{ backgroundImage: `url(${welcomeBg})`, backgroundSize: 'cover' }}>
-        <img src={etheriumImg} alt='frame image' />
-        <h1>Welcome to Sommelier</h1>
-        <h3>The new coprocessor for the Ethereum VM</h3>
-        <a href='#' className='add-liquidity-button'>Add Liquidity</a>
-      </div>
-
-      <div className='main-why'>
-        <h2>
-          Why Sommelier <span>Cellars</span>?
-        </h2>
-        <div className='main-why__content'>
-          {whyData.map((data, index) => (
-            <div className='main-why__content-item' key={`why-title-${index}`}>
-              <h4>{data.title}</h4>
-            </div>
-          ))}
+      <div className='main-welcome-why'>
+        <div className="main-welcome" style={{ backgroundImage: `url(${welcomeBg})`, backgroundSize: 'cover' }}>
+          <img src={etheriumImg} alt='frame image' />
+          <h1>Welcome to Sommelier</h1>
+          <h3>The new coprocessor for the Ethereum VM</h3>
+          <a href='#' className='add-liquidity-button'>Add Liquidity</a>
         </div>
-        <div className='main-why__content'>
-          {whyData.map((data, index) => (
-            <div className='main-why__content-item' key={`why-description-${index}`}>
-              <p>{data.description}</p>
-            </div>
-          ))}
+
+        <div className='main-why'>
+          <h2>
+            Why Sommelier <span>Cellars</span>?
+          </h2>
+          <div className='main-why__content'>
+            {whyData.map((data, index) => (
+              <div className='main-why__content-item' key={`why-title-${index}`}>
+                <h4>{data.title}</h4>
+                <p>{data.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <a name="usecases" id='usecases'></a>
@@ -163,18 +142,122 @@ export default function Main() {
           Use Cases
         </h2>
         <div className='main-usecases__content'>
+          <div className='main-usecases__content-item mobile-visible'>
+            <img src={spiral1Img} />
+          </div>
           <div className='main-usecases__content-item'>
             <h4>Decentralized Portfolio Management</h4>
             <p>Validator set portfolio management with full user funds control.</p>
           </div>
           <div className='main-usecases__content-item'>
-            <img src={spiral1Img} />
+            <img src={spiral1Img} className='desktop-visible'/>
             <h4>Compounding Fees Across AMMs and Networks</h4>
             <p>Automatically compounding fees without need for active intervention.</p>
           </div>
           <div className='main-usecases__content-item'>
             <h4>Automated Portfolio Rebalancing</h4>
             <p>Automatically rebalance portfolio to focus on high yield Liquidity Provider opportunities and exit lower yielding opportunities.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='main-divider'></div>
+
+      <div className='main-cellar'>
+        <div className='main-cellar__left-container'>
+          <p className='text-content'>NEW</p>
+          <h1 className='d-block sommelier'>
+            Sommelier
+          </h1>
+          <h2 className='d-block cellars'>
+            Cellars
+          </h2>
+          <p className='sub-title mt-3'>Check out the Sommelier Blockchain Explorer for other Cellars and performance.</p>
+          <a href="https://app.sommelier.finance" className='launch-button' target="_blank">
+            <span>Add Liquidity</span>
+            <img src={coinsWhiteImg} alt='frame image' className='mr-2'/>
+          </a>
+        </div>
+        <div className='main-cellar__right-container'>
+          <div className='cellar-item'>
+            <div className='cellar-item__header'>
+              <div className='cellar-item__header__badge'>
+                <div className='cellar-badge'>
+                  Highlighted
+                </div>
+              </div>
+              <div className='cellar-item__header__icon'>
+                <img src={ethImg}/>
+                <img src={usdtImg}/>
+              </div>
+              <div className='cellar-item__header__pool'>
+                ETH/USDT
+              </div>
+            </div>
+            <div className='cellar-item__body'>
+              <p>Today's Cellar Performance</p>
+              <span>Past 24 hrs</span>
+              <div className='cellar-pool-detail'>
+                <div className='cellar-pool-detail-title'>
+                  24h APY
+                </div>
+                <div className='cellar-pool-detail-value'>
+                  2.3%
+                </div>
+              </div>
+              <div className='cellar-pool-detail'>
+                <div className='cellar-pool-detail-title'>
+                  24h Volume
+                </div>
+                <div className='cellar-pool-detail-value'>
+                  $1.234M
+                </div>
+              </div>
+            </div>
+            <div className='cellar-item__footer'>
+              <a href='https://app.sommelier.finance/' target='_blank' className='add-liquidity-button'>Add Liquidity</a>
+            </div>
+          </div>
+
+
+          <div className='cellar-item'>
+            <div className='cellar-item__header'>
+              <div className='cellar-item__header__badge'>
+                <div className='cellar-badge'>
+                  Highlighted
+                </div>
+              </div>
+              <div className='cellar-item__header__icon'>
+                <img src={ethImg}/>
+                <img src={usdcImg}/>
+              </div>
+              <div className='cellar-item__header__pool'>
+                ETH/USDC
+              </div>
+            </div>
+            <div className='cellar-item__body'>
+              <p>Today's Cellar Performance</p>
+              <span>Past 24 hrs</span>
+              <div className='cellar-pool-detail'>
+                <div className='cellar-pool-detail-title'>
+                  24h APY
+                </div>
+                <div className='cellar-pool-detail-value'>
+                  2.3%
+                </div>
+              </div>
+              <div className='cellar-pool-detail'>
+                <div className='cellar-pool-detail-title'>
+                  24h Volume
+                </div>
+                <div className='cellar-pool-detail-value'>
+                  $1.234M
+                </div>
+              </div>
+            </div>
+            <div className='cellar-item__footer'>
+              <a href='https://app.sommelier.finance/' target='_blank' className='add-liquidity-button'>Add Liquidity</a>
+            </div>
           </div>
         </div>
       </div>
